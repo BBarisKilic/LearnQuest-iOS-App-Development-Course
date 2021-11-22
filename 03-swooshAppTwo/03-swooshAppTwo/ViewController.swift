3c5de29b5e89a70b6ac838cb464c705538100249
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-11-22 18:56:13 +0100

    Completed swooshAppTwo.

--- 03-swooshAppTwo/03-swooshAppTwo/ViewController.swift
+++ 03-swooshAppTwo/03-swooshAppTwo/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        swoosh.frame = CGRect(x: view.frame.width / 2 - swoosh.frame.width / 2, y: 50, width: swoosh.frame.width, height: swoosh.frame.height)
+        
+        backgroundImage.frame = view.frame
     }
 
 
