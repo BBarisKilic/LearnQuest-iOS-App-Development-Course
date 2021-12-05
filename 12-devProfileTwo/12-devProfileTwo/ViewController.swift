b3b03820a0cd504104e3bd828a9d0dac59e9bb6d
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-12-05 21:43:49 +0100

    Created devProfileTwo.

---
+++ 12-devProfileTwo/12-devProfileTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  11-devProfile
+//
+//  Created by Bülent Barış Kılıç on 1.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
